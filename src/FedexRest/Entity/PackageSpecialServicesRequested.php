6ab93570d1f061f24175a3d284fe6473fa4af445
Ben <jsrgqinbin@gmail.com>
2024-08-26 14:53:15 +0800

    Added Smart Post, Commodities, Customs, Updates to signature requirements.
    Added Smart Post, Commodities, Customs, Updates to signature requirements.

--- src/FedexRest/Entity/PackageSpecialServicesRequested.php
+++ src/FedexRest/Entity/PackageSpecialServicesRequested.php
@@ -2,69 +2,106 @@
 
 namespace FedexRest\Entity;
 
+use FedexRest\Services\Ship\Type\PackageSpecialServiceType;
+
 class PackageSpecialServicesRequested
 {
 
-  public array $specialServiceTypes;
-  public ?DangerousGoodsDetail $dangerousGoodsDetail;
-  public ?Weight $dryIceWeight;
+    public array $specialServiceTypes;
+    public ?DangerousGoodsDetail $dangerousGoodsDetail;
+    public ?Weight $dryIceWeight;
+    public ?$signatureOptionType;
+    public ?PackageSpecialServiceType $dangerousGoods;
 
-  /**
-   * @param array $specialServiceTypes
-   * @return $this
-   */
-  public function setSpecialServiceTypes(array $specialServiceTypes): PackageSpecialServicesRequested
-  {
-    $this->specialServiceTypes = $specialServiceTypes;
-    return $this;
-  }
-
-  /**
-   * @param string $specialServiceType
-   * @return $this
-   */
-  public function addToSpecialServiceTypes(string $specialServiceType): PackageSpecialServicesRequested
-  {
-    $this->specialServiceTypes[] = $specialServiceType;
-    return $this;
-  }
-
-  /**
-   * @param ?DangerousGoodsDetail $dangerousGoodsDetail
-   * @return $this
-   */
-  public function setDangerousGoodsDetail(?DangerousGoodsDetail $dangerousGoodsDetail): PackageSpecialServicesRequested
-  {
-    $this->dangerousGoodsDetail = $dangerousGoodsDetail;
-    return $this;
-  }
-
-  /**
-   * @param ?Weight $dryIceWeight
-   * @return $this
-   */
-  public function setDryIceWeight(?Weight $dryIceWeight): PackageSpecialServicesRequested
-  {
-    $this->dryIceWeight = $dryIceWeight;
-    return $this;
-  }
-
-  public function prepare(): array {
-    $data = [];
-
-    if (!empty($this->setSpecialServiceTypes)) {
-      $data['specialServiceTypes'] = $this->setSpecialServiceTypes;
+    /**
+     * @param  array  $specialServiceTypes
+     * @return $this
+     */
+    public function setSpecialServiceTypes(array $specialServiceTypes): PackageSpecialServicesRequested
+    {
+        $this->specialServiceTypes = $specialServiceTypes;
+        return $this;
     }
 
-    if (!empty($this->dangerousGoodsDetail)) {
-      $data['dangerousGoodsDetail'] = $this->dangerousGoodsDetail->prepare();
+    /**
+     * @param  string  $specialServiceType
+     * @return $this
+     */
+    public function addToSpecialServiceTypes(string $specialServiceType): PackageSpecialServicesRequested
+    {
+        $this->specialServiceTypes[] = $specialServiceType;
+        return $this;
     }
 
-    if (!empty($this->dryIceWeight)) {
-      $data['dryIceWeight'] = $this->dryIceWeight->prepare();
+    /**
+     * @param ?DangerousGoodsDetail  $dangerousGoodsDetail
+     * @return $this
+     */
+    public function setDangerousGoodsDetail(?DangerousGoodsDetail $dangerousGoodsDetail): PackageSpecialServicesRequested
+    {
+        $this->dangerousGoodsDetail = $dangerousGoodsDetail;
+        return $this;
     }
 
-    return $data;
-  }
+    /**
+     * @param ?DangerousGoods $dangerousGoods
+     * @return $this
+     */
+    public function setDangerousGoods(?PackageSpecialServiceType $dangerousGoods): PackageSpecialServicesRequested
+    {
+        $this->dangerousGoods = $dangerousGoods;
+        return $this;
+    }
+
+    /**
+     * @param ?Weight  $dryIceWeight
+     * @return $this
+     */
+    public function setDryIceWeight(?Weight $dryIceWeight): PackageSpecialServicesRequested
+    {
+        $this->dryIceWeight = $dryIceWeight;
+        return $this;
+    }
+
+    /**
+     * @param ?PackageSpecialServiceType $signatureOptionType
+     * @return $this
+     */
+    public function setSignatureOptionType(?PackageSpecialServiceType $signatureOptionType): PackageSpecialServicesRequested {
+        $this->signatureOptionType = $signatureOptionType;
+        return $this;
+
+    }
+
+    public function prepare(): array
+    {
+        $data = [];
+
+        if (!empty($this->specialServiceTypes)) {
+            $data['specialServiceTypes'] = $this->specialServiceTypes;
+        }
+
+        if (!empty($this->dangerousGoods)) {
+            $data['dangerousGoods'] = $this->dangerousGoods;
+        }
+
+        if (!empty($this->setSpecialServiceTypes)) {
+            $data['specialServiceTypes'] = $this->setSpecialServiceTypes;
+        }
+
+        if (!empty($this->dangerousGoodsDetail)) {
+            $data['dangerousGoodsDetail'] = $this->dangerousGoodsDetail->prepare();
+        }
+
+        if (!empty($this->dryIceWeight)) {
+            $data['dryIceWeight'] = $this->dryIceWeight->prepare();
+        }
+
+        if (!empty($this->signatureOptionType)) {
+            $data['signatureOptionType'] = $this->signatureOptionType;
+        }
+
+        return $data;
+    }
 
 }
--- src/FedexRest/Services/Rates/CreateRatesRequest.php
+++ src/FedexRest/Services/Rates/CreateRatesRequest.php
@@ -12,6 +12,8 @@ use FedexRest\Services\AbstractRequest;
 use FedexRest\Services\Ship\Entity\Label;
 use FedexRest\Services\Ship\Entity\ShipmentSpecialServices;
 use FedexRest\Services\Ship\Entity\ShippingChargesPayment;
+use FedexRest\Services\Ship\Entity\SmartPostInfoDetail;
+use FedexRest\Services\Ship\Entity\CustomsClearanceDetail;
 use GuzzleHttp\Exception\GuzzleException;
 
 class CreateRatesRequest extends AbstractRequest
@@ -24,10 +26,12 @@ class CreateRatesRequest extends AbstractRequest
     protected array $rateRequestTypes;
     protected string $packagingType = '';
     protected string $pickupType = '';
+    protected ?SmartPostInfoDetail $smart_post;
     protected int $accountNumber;
     protected array $lineItems = [];
     protected ShipmentSpecialServices $shipmentSpecialServices;
     protected ShippingChargesPayment $shippingChargesPayment;
+    protected ?CustomsClearanceDetail $customsClearanceDetail;
     protected int $totalWeight;
     protected string $preferredCurrency = '';
     protected int $totalPackageCount;
@@ -155,6 +159,15 @@ class CreateRatesRequest extends AbstractRequest
     }
 
     /**
+     * @param SmartPostInfoDetail $SmartPostInfoDetail
+     * @return $this
+     */
+    public function setSmartPost(SmartPostInfoDetail $smartPost): CreateRatesRequest {
+        $this->smart_post = $smartPost;
+        return $this;
+    }
+
+    /**
      * @param int $accountNumber
      * @return $this
      */
@@ -228,6 +241,15 @@ class CreateRatesRequest extends AbstractRequest
     public function getShippingChargesPayment(): ShippingChargesPayment
     {
         return $this->shippingChargesPayment;
+    }
+
+    /**
+     * @param CustomsClearanceDetail $customsClearanceDetail
+     * @return $this
+     */
+    public function setCustomsClearanceDetail(CustomsClearanceDetail $customsClearanceDetail) {
+        $this->customsClearanceDetail = $customsClearanceDetail;
+        return $this;
     }
 
     /**
@@ -306,24 +328,24 @@ class CreateRatesRequest extends AbstractRequest
         return $this->servicesNeededOnRateFailure;
     }
 
-    public function setVariableOptions(?string $variableOptions): CreateRatesRequest
+    public function setVariableOptions(string $variableOptions): CreateRatesRequest
     {
         $this->variableOptions = $variableOptions;
         return $this;
     }
 
-    public function getVariableOptions(): ?string
+    public function getVariableOptions(): string
     {
         return $this->variableOptions;
     }
 
-    public function setRateSortOrder(?string $rateSortOrder): CreateRatesRequest
+    public function setRateSortOrder(string $rateSortOrder): CreateRatesRequest
     {
         $this->rateSortOrder = $rateSortOrder;
         return $this;
     }
 
-    public function getRateSortOrder(): ?string
+    public function getRateSortOrder(): string
     {
         return $this->rateSortOrder;
     }
@@ -354,6 +376,7 @@ class CreateRatesRequest extends AbstractRequest
         $data = [
             'returnTransitTimes' => $this->returnTransitTimes,
             'servicesNeededOnRateFailure' => $this->servicesNeededOnRateFailure,
+            'smartPostInfoDetail' => $this->smart_post,
         ];
 
         if ($this->variableOptions) {
@@ -387,6 +410,10 @@ class CreateRatesRequest extends AbstractRequest
 
         if (!empty($this->shipmentSpecialServices)) {
             $data['shipmentSpecialServices'] = $this->shipmentSpecialServices->prepare();
+        }
+
+        if (!empty($this->customsClearanceDetail)) {
+            $data['customsClearanceDetail'] = $this->customsClearanceDetail->prepare();
         }
 
         if (!empty($this->serviceType)) {
