656685ae10424ec4aa0520c8605f9015b9c4ab9d
Ben <jsrgqinbin@gmail.com>
2024-09-21 17:11:51 +0800

    add THIRD_PARTY
    add THIRD_PARTY

--- src/FedexRest/Services/Ship/Entity/ShippingChargesPayment.php
+++ src/FedexRest/Services/Ship/Entity/ShippingChargesPayment.php
@@ -5,8 +5,10 @@ namespace FedexRest\Services\Ship\Entity;
 class ShippingChargesPayment
 {
     public ?string $paymentType;
+    public ?string $accountNumber;
+
     /**
-     * @param string  $paymentType
+     * @param  string  $paymentType
      * @return $this
      */
     public function setPaymentType(string $paymentType): ShippingChargesPayment
@@ -15,11 +17,26 @@ class ShippingChargesPayment
         return $this;
     }
 
+    /**
+     * @param  string  $accountNumber
+     * @return $this
+     */
+    public function setThirdPartyAccountNumber(string $accountNumber): ShippingChargesPayment
+    {
+        $this->accountNumber = $accountNumber;
+        return $this;
+    }
+
     public function prepare(): array
     {
         $data = [];
         if (!empty($this->paymentType)) {
             $data['paymentType'] = $this->paymentType;
+        }
+        if (!empty($this->accountNumber)) {
+            $data['payor'] = [
+                'responsibleParty' => ['accountNumber' => $this->accountNumber],
+            ];
         }
         return $data;
     }
